d4de72b1749342355050693114f1205529c4d734
Kaccady <z-rz@mail.com>
2019-06-08 02:19:12 +0300

    add widget

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import sun from './sunny.svg';
+import moon from './night.svg';
 
+const currentTime = new Date().getHours();
 function App() {
+  const [weather, setWeather] = useState("");
+  if (weather === "") {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?id=536203&APPID=4b5cb159efdd504a26993167524002db"
+      )
+      .then(res => setWeather(res));
+  }
+  const [isDark, setIsDark] = useState(currentTime < 7 ? true : false);
+  if(weather!==''){
+    console.log(weather.data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className={isDark ? "weather dark" : "weather light"}
+        onClick={() => setIsDark(!isDark)}
+      >
+        <img src={isDark?moon:sun} alt="pic here" className='ico' />
+        <div className="text-block">
+          <p className="sity-name">{weather.data.name}</p>
+          <p className="temp">{(weather.data.main.temp-273).toFixed()+'°C'}</p>
+        </div>
+      </div>
     </div>
   );
-}
+}else{return null}}
 
 export default App;
